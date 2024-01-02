afbe9e73a4c9aa6bc5efdebb88c7f09e607d9f55
hamzakhribech <hamzakhribech45@gmail.com>
2024-01-02 21:10:51 +0100

    continue auth logique in login

--- src/Store/authSlice.js
+++ src/Store/authSlice.js
@@ -1,17 +1,46 @@
 // reducers/counterSlice.js
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+  hi:"hi",
+  isLogged: localStorage.getItem('isLogged') === 'true',
+  userId: localStorage.getItem('userId') || null,
+  userImg: localStorage.getItem('userImg') || null,
+  userName: localStorage.getItem('userName') || '',
+  email: localStorage.getItem('email') || '',
+  token: localStorage.getItem('token') || '',
+  isAdmin: localStorage.getItem('isAdmin') === 'true',
+};
 const authSlice = createSlice({
   name: 'auth',
-  initialState:{auth:"hello redux working"},
+  initialState:initialState,
   reducers: {
     login: (state, action) => {
-      state.isAuthenticated = true;
-      state.user = action.payload;
+      state.isLogged = true;
+      state.hi="hello"
+      state.userId = action.payload.userId;
+      state.userImg = action.payload.userImg;
+      state.userName = action.payload.userName;
+      state.email = action.payload.email;
+      state.token = action.payload.token;
+      state.isAdmin = action.payload.isAdmin
+      
+      window.localStorage.setItem("token", state.token);
+      window.localStorage.setItem("userId", state.userId);
+      window.localStorage.setItem("userImg", state.userImg);
+      window.localStorage.setItem("userName", state.userName);
+      window.localStorage.setItem("email", state.email);
+      window.localStorage.setItem("isAdmin", state.isAdmin);
+      window.localStorage.setItem("isLoged", state.isLogged);
+
+      console.log(state);
+      console.log("done in redux state");
     },
-    decrement: state => state - 1,
+    init: (state, action) => { 
+
+    }
   },
 });
 
-export const authActions = authSlice.actions;
+export const {login} = authSlice.actions;
 export default authSlice.reducer;
